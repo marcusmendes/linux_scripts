1d11d073e33b93ff4c3b4c8a7eb300fd692339e9
Marcus Mendes Maciel <mendesxx.ti@hotmail.com>
2020-09-28 21:47:50 -0300

    Atualização da url para instalar o docker compose

--- install_docker.sh
+++ install_docker.sh
@@ -25,7 +25,7 @@ echo "ADD YOUR USER TO DOCKER GROUP"
 sudo usermod -aG docker $USER
 
 echo "INSTALL DOCKER COMPOSE"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
